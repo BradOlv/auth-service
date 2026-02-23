76813ad48cea59aeb619570e59d93be16f2cb9a3
BradOlv <boliva-2023502@kinal.edu.gt>
2026-02-23 09:13:04 -0600

    se ha implementado y trabajado el repositorio de usuarios con métodos core y generador de UUID

--- src/AuthService.Domain/Interfaces/IUserRepository.cs
+++ src/AuthService.Domain/Interfaces/IUserRepository.cs
@@ -1,18 +1,18 @@
 using AuthService.Domain.Entities;
+
 namespace AuthService.Domain.Interfaces;
 
 public interface IUserRepository
 {
+    Task<User> CreateAsync(User user);
     Task<User?> GetByIdAsync(string id);
     Task<User?> GetByEmailAsync(string email);
-    Task<User?> getByUsernameAsync(string username);
+    Task<User?> GetByUsernameAsync(string username);
+    Task<User?> GetByEmailVerificationTokenAsync(string token);
     Task<User?> GetByPasswordResetTokenAsync(string token);
-    Task <User> CreateAsync(User user);
-    Task <User> UpdateAsync(User user);
-    Task <User> DeleteAsync(User user);
-
     Task<bool> ExistsByEmailAsync(string email);
     Task<bool> ExistsByUsernameAsync(string username);
+    Task<User> UpdateAsync(User user);
+    Task<bool> DeleteAsync(string id);
     Task UpdateUserRoleAsync(string userId, string roleId);
-    
 }
